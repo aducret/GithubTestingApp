b7dd5b92cce2e526bb9d85239790b085931a5c7f
aducret <argentino.ducret@wolox.com.ar>
2018-08-18 20:03:43 -0300

    Added Swinject and HomeViewController

---
+++ GitTestingApp/Screens/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  GitTestingApp
+//
+//  Created by Argentino Ducret on 18/08/2018.
+//  Copyright © 2018 Argentino Ducret. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
